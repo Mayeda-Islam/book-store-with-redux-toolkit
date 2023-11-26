fab21ce86f90e2e8c575ea02b7a5060ba92b64b8
unknown <mayedakonika@gmail.com>
2023-11-27 00:24:00 +0600

    filterslice added

--- src/components/navbar/tags/Tag.js
+++ src/components/navbar/tags/Tag.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useDebugValue } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { tagRemove, tagSelected } from "../../../features/filter/filterSlice";
 
 const Tag = ({ tag }) => {
-  const { title } = tag;
+  const { tags } = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
+  let selected = tags.includes(tag) ? true : false;
+
+  const handleSelect = () => {
+    if (selected) {
+      dispatch(tagRemove(tag));
+    } else dispatch(tagSelected(tag));
+  };
+  console.log(selected);
+  const style = selected
+    ? "bg-blue-600 text-blue-100 px-4 py-1 rounded-full cursor-pointer"
+    : "bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer";
   return (
     <>
-      <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
-        {title}
+      <div className={style} onClick={handleSelect}>
+        {tag}
       </div>
     </>
   );
@@ -13,7 +27,7 @@ const Tag = ({ tag }) => {
 
 export default Tag;
 
-// <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+// <div className="">
 // react
 // </div>
 // <div className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
---
+++ src/features/filter/filterSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  tags: [],
+  search: "",
+};
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    tagSelected: (state, action) => {
+      state.tags.push(action.payload);
+    },
+    tagRemove: (state, action) => {
+      const currentIndex = state.tags.indexOf(action.payload);
+      console.log(currentIndex);
+      if (currentIndex !== -1) {
+        state.tags.splice(currentIndex, 1);
+      }
+    },
+    searched: (state, action) => {
+      state.search = action.payload;
+    },
+  },
+});
+export default filterSlice.reducer;
+export const { tagSelected, tagRemove, searched } = filterSlice.actions;
